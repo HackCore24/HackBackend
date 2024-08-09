06c7911d2e8ffe07a03cba773fa3ae9e037f462d
SayderG <zimens05@mail.ru>
2024-08-09 21:04:09 +0300

    path fix

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ app = FastAPI(title="HackBackend API",
               })
 app.add_middleware(ApidocBasicAuthMiddleware)
 
-
 origins = ["http://localhost:3000", "http://localhost:5500"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*", "OPTIONS"],
@@ -26,8 +25,6 @@ app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True
 router = APIRouter()
 
 router.include_router(router=api_router)
-
-app.include_router(router)
 
 
 @router.get("/ping", tags=["Server"])
